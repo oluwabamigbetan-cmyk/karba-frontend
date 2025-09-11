92720c778b3da091ebe64714c700ea16fffc05ef
oluwabamigbetan-cmyk <oluwabamigbetan@gmail.com>
2025-09-11 21:42:46 +0100

    config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 window.KARBA_CONFIG = {
   BACKEND_URL: 'https://karba-backend-jgbp.onrender.com',
   // ↓ paste YOUR reCAPTCHA SITE key (the blue one) here
-  RECAPTCHA_SITE_KEY: '6LcB58QPAAAAAARTZk6eZGGJXXdZwgzaUn4dg2C'
+  RECAPTCHA_SITE_KEY: '6LfKqMYrAAAAAETFiKHQxlo90x6XuCMcPRBk2vi5'
 };
