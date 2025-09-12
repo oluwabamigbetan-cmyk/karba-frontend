5f044c714d6cd8891d33483e7ea156c29afdc3f5
oluwabamigbetan-cmyk <oluwabamigbetan@gmail.com>
2025-09-12 21:09:27 +0100

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
-// config.js
+// config.v2.js
 window.KARBA_CONFIG = {
-  BACKEND_URL: 'https://karba-backend-jgbp.onrender.com',
-  // Replace with your current reCAPTCHA v3 **Site Key**
-  RECAPTCHA_SITE_KEY: '6Ld66MYrAAAAAEdGo5j3wHH9YbIPQFlWKXkN4dqc'
+  BACKEND_URL: "https://karba-backend-jgbp.onrender.com",
+  RECAPTCHA_SITE_KEY: "6Ld66MYrAAAAAEdGo5j3wHH9YbIPQFlWKXkN4dqc"  // from reCAPTCHA v3 Admin
 };
