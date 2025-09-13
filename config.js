3331b6848169253fcf80bc7e75a711e711b9b604
oluwabamigbetan-cmyk <oluwabamigbetan@gmail.com>
2025-09-13 18:08:04 +0100

    config.js

--- config.js
+++ config.js
@@ -1,10 +1,5 @@
 window.KARBA_CONFIG = {
-  // 1) Your backend base URL (NO trailing slash)
   BACKEND_URL: "https://karba-backend-jgbp.onrender.com",
-
-  // 2) Your reCAPTCHA v3 SITE KEY (from Google reCAPTCHA Admin Console)
-  RECAPTCHA_SITE_KEY: "6Ld66MYmAAAAAEdo53jWHt9YDfpUWKhXNkW4k4qc"
+  RECAPTCHA_SITE_KEY: "6Ld66MYrAAAAAEdGo5j3wHH9YbIPQFlWKXkN4dqc"
 };
-
-// Sanity log (you should see an object in DevTools console)
 console.log("[KARBA_CONFIG]", window.KARBA_CONFIG);
