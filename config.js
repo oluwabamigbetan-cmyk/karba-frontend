1fc5b8e26bf305b66decf89508132462a2b96ce4
oluwabamigbetan-cmyk <oluwabamigbetan@gmail.com>
2025-09-14 18:21:00 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
-// CONFIG — edit these two values only
+// config.js
 window.KARBA_CONFIG = {
-  BACKEND_URL: "https://karba-backend-jgbp.onrender.com", // or https://api.karbafinancialconsultant.com
-  RECAPTCHA_SITE_KEY: "6Ld66MYrAAAAAEdGo5j3wHH9YbIPQFlWKXkN4dqc" // <-- your real site key
+  // NO trailing slash
+  BACKEND_URL: "https://karba-backend-jgbp.onrender.com",
+
+  // Your reCAPTCHA v3 SITE key (must match your domain(s) in Google admin)
+  RECAPTCHA_SITE_KEY: "6Ld66MYrAAAAAEdGo5J3whH9YbIDPQfLWKXN4dqc"
 };
+
 console.log("[KARBA_CONFIG]", window.KARBA_CONFIG);
