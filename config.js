e2f2290235b5faaf17628242ee23baf0694c32b2
oluwabamigbetan-cmyk <oluwabamigbetan@gmail.com>
2025-09-14 03:39:19 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,5 @@
-// config.js — EDIT THESE TWO VALUES ONLY
 window.KARBA_CONFIG = {
-  // 1) Your backend base URL (NO trailing slash)
   BACKEND_URL: "https://karba-backend-jgbp.onrender.com",
-
-  // 2) Your reCAPTCHA v3 SITE KEY (for your Vercel domain)
-  RECAPTCHA_SITE_KEY: "6Ld66MYmAAAAAEdGo5j3whH9YIbIPQfLWKXkN4dqc",
+  RECAPTCHA_SITE_KEY: "6Ld66MYrAAAAAEdGo5j3wHH9YbIPQFlWKXkN4dqc"
 };
-
-// sanity log (you should see an object in DevTools console)
-console.log('[KARBA_CONFIG]', window.KARBA_CONFIG);
+console.log("[KARBA_CONFIG]", window.KARBA_CONFIG);
