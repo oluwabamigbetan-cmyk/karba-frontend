6ed106a3268bc25d84e039e8f1ef9b2f368a5bbb
oluwabamigbetan-cmyk <oluwabamigbetan@gmail.com>
2025-09-15 08:50:34 +0100

    config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
-// NO trailing slash on BACKEND_URL
-window.KARBA_CONFIG = {
-  BACKEND_URL: 'https://karba-backend-jgbp.onrender.com',
-  RECAPTCHA_SITE_KEY: '6Ld66MYrAAAAAEdGo5J3whH9YbIDPQfLwWCKN4dqc'
-};
-
-console.log('KARBA_CONFIG', window.KARBA_CONFIG);
+<script>
+  
+  window.KARBA_CONFIG = {
+    BACKEND_URL: "https://karba-backend-jgbp.onrender.com",
+    RECAPTCHA_SITE_KEY: "6Ld66MYrAAAAAEdGo5j3wHH9YbIPQFlWKXkN4dqc"
+  };
+  console.log("KARBA_CONFIG", window.KARBA_CONFIG);
+</script>
