dd2d8b072c8a5871af71840b636e4d79de4fb1b1
oluwabamigbetan-cmyk <oluwabamigbetan@gmail.com>
2025-09-15 14:59:06 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // PURE JS — no <script> tags here.window.KARBA_CONFIG = {
   BACKEND_URL: "https://karba-backend-jgbp.onrender.com",
-  RECAPTCHA_SITE_KEY: "6LeuMMorAAAAAHKeKwmYN9gzPpvgZNW3hn7FExx9"
+  RECAPTCHA_SITE_KEY: "6LeuMMorAAAAAAhNzhIfYY8IkazL12EJc7fGL8Gc"
 };
 console.log("KARBA_CONFIG", window.KARBA_CONFIG);
